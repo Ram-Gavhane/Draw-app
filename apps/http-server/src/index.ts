2fddb7ffd5eb4f1632d25c31ed363746198a9d93
Ram Gavhane <ramgavhane28@gmail.com>
2025-07-09 19:12:47 +0530

    Signup and login endpoint

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -3,53 +3,90 @@ import * as z from "zod/v4"
 import { userSchema } from "./utils/zodSchema";
 import { middleware } from "./middleware/middleware";
 import jwt from "jsonwebtoken";
-
+import 'dotenv/config';
+import dotenv from "dotenv";
+dotenv.config();
+import { JWT_SECRET } from "./config";
+import { client } from "@repo/db/client";
+import bcrypt from "bcrypt";
 
 const app = express();
 app.use(express.json());
 
-app.post("/api/v1/signup",function (req,res){
+app.post("/api/v1/signup", async function (req,res){
     const body = req.body;
     const User =  z.object({
         username:z.string(),
         email:z.email(),
         password:z.string()
     });
-    try{
-        User.parse(body);
+    const parsedData = User.safeParse(body);
+    if(parsedData.success){
+        const hashedPassword = await bcrypt.hash(parsedData.data.password,3);
+        const response = await client.user.create({
+            data:{
+                username: body.username,
+                email: body.email,
+                password: hashedPassword
+            }
+        })
+    }else{
         res.json({
-            "message":"Signed Up"
-        });
-    }catch(err){
-        if(err instanceof z.ZodError){
-        console.log(err.issues);
-        console.log(err);
-        }
+            "message":"Invalid inputs"
+        })
+        return;
     }
+    res.json({
+        "message":"Signed Up"
+    });
 });
 
-app.post("/api/v1/login",function(req, res){
+app.post("/api/v1/login",async function(req, res){
     const body = req.body;
     const User =  z.object({
         username:z.string(),
         password:z.string()
     });
     const userData = User.safeParse(body);
-    if(! userData.success){
+    if(!userData.success){
         res.send(userData.error);
     }else{
-        const token = jwt.sign(userData.data.username, "secret");
-        res.json({
-            "message":"You are logged in",
-            "Token":token
-        });
+        const user = await client.user.findFirst({
+            where:{
+                username: userData.data.username
+            }
+        })
+        if(!user){
+            res.json({
+                "message":"User with this username doesn't exist"
+            });
+            return;
+        }
+        const passCheck = await bcrypt.compare(userData.data.password,user.password)
+        if(passCheck){
+            const token = jwt.sign(userData.data.username, JWT_SECRET);
+            res.json({
+                "message":"You are logged in",
+                "Token":token
+            });
+        }else{
+            res.status(411).json({
+                "message":"Incorrect Password"
+            });
+        }
     }
-
 });
 
 app.use(middleware)
 
-app.get("/", function(req, res){
+app.post("/create-room", function(req, res){
+    const name = req.userId;
+    res.json({
+        "username":name
+    });
+});
+
+app.post("/join-room", function(req, res){
     const name = req.userId;
     res.json({
         "username":name
--- apps/http-server/src/middleware/middleware.ts
+++ apps/http-server/src/middleware/middleware.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "../config";
 
 export function middleware(req:Request, res:Response, next:NextFunction){
     const token = req.headers.authorization;
 
     try{
-        const decodeData = jwt.verify(token as string, "secret");
+        const decodeData = jwt.verify(token as string, JWT_SECRET);
         if(decodeData){
             console.log(decodeData)
             req.userId = decodeData as string;
